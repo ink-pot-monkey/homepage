52a921cc1f2826ca6871267268c6e7f8ad0b4c7c
thomasseandominickelly <tsdkelly@gmail.com>
2020-06-06 13:39:23 +0100

    spring cleaning

--- src/data/siteConfig.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteTitle: "Thomas Sean Dominic Kelly",
-  siteDescription: `This is my homepage acting as a portfolio for my developer skillset`,
-  keyWords: ["gatsbyjs", "react"],
-  authorName: "Thomas Sean Dominic Kelly",
-  githubUsername: "thomasseandominickelly",
-}
