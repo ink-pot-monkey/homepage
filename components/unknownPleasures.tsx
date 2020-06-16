7ba5af69ae6fdfa2884476ead4b4fd8500d290bf
thomasSDK <tsdkelly@gmail.com>
2020-06-16 16:36:56 +0100

    fixed canvas size on retina

--- components/unknownPleasures.tsx
+++ components/unknownPleasures.tsx
@@ -3,9 +3,9 @@ import { makeNoise2D } from "open-simplex-noise";
 import { lineColor, padding, backgroundColor } from "../components/layout";
 
 interface line {
-  x: number;
-  y: number;
-  variance: number;
+    x: number;
+    y: number;
+    variance: number;
 }
 
 const noise2D = makeNoise2D(Date.now());
@@ -13,131 +13,139 @@ let yoff = 0;
 let xoff = 0;
 
 export default function UnknownPleasures() {
-  let canvasRef = useRef<HTMLCanvasElement>(null);
+    let canvasRef = useRef<HTMLCanvasElement>(null);
 
-  useEffect(() => {
-    let requestID: number;
+    useEffect(() => {
+        let requestID: number;
 
-    if (canvasRef.current) {
-      const canvas = canvasRef.current;
-      const body: HTMLElement | null = document.querySelector("body");
-      if (!body) return;
+        if (canvasRef.current) {
+            const canvas = canvasRef.current;
+            const body: HTMLElement | null = document.querySelector("body");
+            if (!body) return;
 
-      const width =
-        parseFloat(getComputedStyle(body, null).width.replace("px", "")) -
-        padding;
-      const height =
-        parseFloat(getComputedStyle(body, null).height.replace("px", "")) -
-        padding;
-      const dpr = window.devicePixelRatio;
-      const context = canvas.getContext("2d");
+            const width = window.innerWidth - padding;
+            const height = window.innerHeight - padding;
 
-      // Put this in a function to allow us to reset the canvas
-      const canvasSetup = () => {
-        canvas.width = width * dpr;
-        canvas.height = height * dpr;
-        if (context) {
-          context.scale(dpr, dpr);
-          context.lineWidth = 2;
-        }
-      };
-      // Initial setup
-      canvasSetup();
+            canvas.style.width = width + "px";
+            canvas.style.height = height + "px";
 
-      let step = 25;
-      let offset = 150;
+            console.log(width, height);
+            const dpr = window.devicePixelRatio;
+            const context = canvas.getContext("2d");
 
-      // Plot the original lines
-      const lines: line[][] = [];
-      // need to change i so that it is adaptive to height of screen
-      for (var i = 600; i <= width - step; i += step) {
-        let line = [];
-        for (var j = 0; j < width + step; j += step) {
-          let distanceToCenter = Math.abs(j - width / 2);
-          let variance = Math.max(width / 2 - offset - distanceToCenter, 0);
-          let random = ((Math.random() * variance) / 2) * -1;
-          let point = { x: j, y: i + random, variance: variance };
-          // Point Plotter
-          // if (context) {
-          //   context.beginPath();
-          //   context.arc(point.x, point.y, 1, 0, 2 * Math.PI);
-          //   context.stroke();
-          // }
-          line.push(point);
-        }
-        lines.push(line);
-      }
+            // Put this in a function to allow us to reset the canvas
+            const canvasSetup = () => {
+                canvas.width = width * dpr;
+                canvas.height = height * dpr;
+                if (context) {
+                    context.scale(dpr, dpr);
+                    context.lineWidth = 1.5;
+                }
+            };
 
-      const DrawLines = () => {
-        if (context) {
-          for (let i = 0; i < lines.length; i++) {
-            context.beginPath();
-            context.moveTo(lines[i][0].x, lines[i][0].y);
+            let step = 25;
+            let offset = 150;
 
-            for (var j = 0; j < lines[i].length - 2; j++) {
-              var xc = (lines[i][j].x + lines[i][j + 1].x) / 2;
-              var yc = (lines[i][j].y + lines[i][j + 1].y) / 2;
-              context.quadraticCurveTo(lines[i][j].x, lines[i][j].y, xc, yc);
+            // Plot the original lines
+            const lines: line[][] = [];
+            // need to change i so that it is adaptive to height of screen
+            for (var i = 600; i <= width - step; i += step) {
+                let line = [];
+                for (var j = 0; j < width + step; j += step) {
+                    let distanceToCenter = Math.abs(j - width / 2);
+                    let variance = Math.max(
+                        width / 2 - offset - distanceToCenter,
+                        0
+                    );
+                    let random = ((Math.random() * variance) / 2) * -1;
+                    let point = { x: j, y: i + random, variance: variance };
+                    // Point Plotter
+                    // if (context) {
+                    //   context.beginPath();
+                    //   context.arc(point.x, point.y, 1, 0, 2 * Math.PI);
+                    //   context.stroke();
+                    // }
+                    line.push(point);
+                }
+                lines.push(line);
             }
 
-            context.quadraticCurveTo(
-              lines[i][j].x,
-              lines[i][j].y,
-              lines[i][j + 1].x,
-              lines[i][j + 1].y
-            );
-            context.save();
-            context.globalCompositeOperation = "destination-out";
-            context.fill();
-            context.restore();
-            context.strokeStyle = lineColor;
-            context.stroke();
-          }
+            const DrawLines = () => {
+                if (context) {
+                    for (let i = 0; i < lines.length; i++) {
+                        context.beginPath();
+                        context.moveTo(lines[i][0].x, lines[i][0].y);
+
+                        for (var j = 0; j < lines[i].length - 2; j++) {
+                            var xc = (lines[i][j].x + lines[i][j + 1].x) / 2;
+                            var yc = (lines[i][j].y + lines[i][j + 1].y) / 2;
+                            context.quadraticCurveTo(
+                                lines[i][j].x,
+                                lines[i][j].y,
+                                xc,
+                                yc
+                            );
+                        }
+
+                        context.quadraticCurveTo(
+                            lines[i][j].x,
+                            lines[i][j].y,
+                            lines[i][j + 1].x,
+                            lines[i][j + 1].y
+                        );
+                        context.save();
+                        context.globalCompositeOperation = "destination-out";
+                        context.fill();
+                        context.restore();
+                        context.strokeStyle = lineColor;
+                        context.stroke();
+                    }
+                }
+            };
+
+            DrawLines();
+
+            const animate = () => {
+                xoff -= 10 * lines.length;
+                lines.forEach((line, index) => {
+                    yoff -= 10 * lines[0].length;
+                    xoff += 0.0001;
+                    line.forEach((point) => {
+                        yoff += 0.00001;
+                        point.y +=
+                            (noise2D(xoff, yoff) * point.variance) / 1000; // * 1000;
+                        yoff += 10;
+                    });
+                    xoff += 10;
+                });
+                canvasSetup();
+                DrawLines();
+                requestID = requestAnimationFrame(animate);
+            };
+
+            animate();
         }
-      };
 
-      DrawLines();
+        return () => {
+            cancelAnimationFrame(requestID);
+        };
+    }, []);
 
-      const animate = () => {
-        xoff -= 10 * lines.length;
-        lines.forEach((line, index) => {
-          yoff -= 10 * lines[0].length;
-          xoff += 0.0001;
-          line.forEach((point) => {
-            yoff += 0.00001;
-            point.y += (noise2D(xoff, yoff) * point.variance) / 1000; // * 1000;
-            yoff += 10;
-          });
-          xoff += 10;
-        });
-        canvasSetup();
-        DrawLines();
-        requestID = requestAnimationFrame(animate);
-      };
+    return (
+        <>
+            <canvas ref={canvasRef} className="canvas" />
 
-      animate();
-    }
-
-    return () => {
-      cancelAnimationFrame(requestID);
-    };
-  });
-
-  return (
-    <>
-      <canvas ref={canvasRef} className="canvas" />
-
-      <style jsx>{`
-        .canvas {
-          position: absolute;
-          left: 0;
-          right: 0;
-          top: 0;
-          bottom: 0;
-          margin: auto;
-          background-color: ${backgroundColor};
-        }
-      `}</style>
-    </>
-  );
+            <style jsx>{`
+                .canvas {
+                    position: absolute;
+                    left: 0;
+                    right: 0;
+                    top: 0;
+                    bottom: 0;
+                    margin: auto;
+                    background-color: ${backgroundColor};
+                }
+            `}</style>
+        </>
+    );
 }
